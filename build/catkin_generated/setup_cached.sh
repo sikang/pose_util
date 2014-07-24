d49a3932ad927180deca29193add9fdd53dce4ce
Sikang <lskwdlskwd@gmail.com>
2014-07-24 14:47:46 -0400

    pose_util

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/sikang/ros/multirobotslam/pose_utils/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/sikang/ros/multirobotslam/pose_utils/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/sikang/ros/multirobotslam/pose_utils/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/sikang/ros/multirobotslam/pose_utils/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/sikang/ros/multirobotslam/pose_utils/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/sikang/ros/multirobotslam/pose_utils/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/sikang/ros/multirobotslam/pose_utils/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/sikang/ros/multirobotslam/pose_utils/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/sikang/ros/multirobotslam/pose_utils/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/sikang/ros/multirobotslam/pose_utils:/opt/ros/hydro/share:/opt/ros/hydro/stacks"
